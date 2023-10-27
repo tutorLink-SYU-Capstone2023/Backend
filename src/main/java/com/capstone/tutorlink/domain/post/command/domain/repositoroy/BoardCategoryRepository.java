acbbaddbedaf860105b6c4d18ffe00781b1ac773
kokojihye <jihyevivianako@gmail.com>
2023-10-27 14:01:08 +0900

    feat : 게시글 등록(추가)

--- src/main/java/com/capstone/tutorlink/domain/post/command/domain/repositoroy/BoardCategoryRepository.java
+++ src/main/java/com/capstone/tutorlink/domain/post/command/domain/repositoroy/BoardCategoryRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 @Repository
 public interface BoardCategoryRepository extends JpaRepository<BoardCategory, String> {
-    @Query(value = "SELECT tutorlink.BOARD_CATEGORY.category_code, tutorlink.BOARD_CATEGORY.category_name "+"ORDER BY category_code ASC", nativeQuery = true)
+    @Query(value =
+            "SELECT category_code, category_name " +
+            "FROM BOARD_CATEGORY "+
+            "ORDER BY category_code ASC", nativeQuery = true)
+    //Query method를 검색해서 활용해보기( 더 좋은 방식이다.)
     public List<BoardCategory> findAllCategory();
 }
