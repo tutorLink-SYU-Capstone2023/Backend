afa10fa507991e2d113ebd16bf830193308d02c0
kokojihye <jihyevivianako@gmail.com>
2023-10-27 14:01:08 +0900

    feat : 게시글 등록(추가)

--- src/main/java/com/capstone/tutorlink/domain/main/query/controller/MainController.java
+++ src/main/java/com/capstone/tutorlink/domain/main/query/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.capstone.tutorlink.domain.main.controller;
+package com.capstone.tutorlink.domain.main.query.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
