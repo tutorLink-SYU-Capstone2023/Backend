e72def867979129ec9ac7b5b56d7d1e9923189a1
kim <kimsquid110@naver.com>
2023-11-17 00:46:44 +0900

    insert 구문 추가 3

--- src/main/resources/sql/dml.sql
+++ src/main/resources/sql/dml.sql
@@ -157,3 +157,37 @@ VALUES
     ('univ_code032', '한경대학교', 'RE001'),
     ('univ_code033', '경인교육대학교', 'RE001'),
     ('univ_code034', '한국교통대학교', 'RE001');
+
+INSERT INTO ACCEPTEDTYPE_CATEGORY (`my_key`, `Field`)VALUES    (1, '정시'),    (2, '수시-학종'),    (3, '수시-교과'),    (4, '적성'),    (5, '논술');
+
+INSERT INTO authority (authority_name, authority_desc)    VALUES    ('ROLE_ADMIN','관리자'),    ('ROLE_TUTEE','튜티'),    ('ROLE_TUTOR','튜터'),    ('ROLE_BORH', '튜티&튜터');
+
+INSERT INTO MEMBER_ROLE (authority_num,member_id)VALUES    (1,'10000000'),    (2,'10000001'),    (2,'10000002'),    (3,'10000003'),    (3,'10000004'),    (3,'10000005'),    (2,'10000006');
+
+INSERT INTO PREFER_REGION (my_key,region_code, authority_num, member_no)VALUES    ('','GANGDONG', 2 , '10000001'),    ('','SONGPA', 2 , '10000002'),    ('','GANGNAM', 3 , '10000001'),    ('','SECHO', 2 , '10000003');
+
+INSERT INTO SUBJECT_MAINCATEGORY (subject_code, subject_name)VALUES    ('sub001', '국어'),    ('sub002', '수학'),    ('sub003', '영어'),    ('sub004', '사회'),    ('sub005', '과학');
+
+INSERT INTO SUBJECT_SUBCATEGORY (my_key, Field, subject_code)VALUES    ('sub001', 'subd001', '문법'),    ('sub001', 'subd002', '비문학'),    ('sub001', 'subd003', '문학'),    ('sub002', 'subd001', '수1'),    ('sub002', 'subd002', '수2'),    ('sub002', 'subd003', '확률과 통계'),    ('sub002', 'subd004', '미적분'),    ('sub002', 'subd005', '기하와 벡터');
+
+INSERT INTO REGION (main_region_code, main_region_name)VALUES    ('1', '서울특별시'),    ('2', '부산광역시'),    ('3', '대구광역시'),    ('4', '인천광역시'),    ('5', '광주광역시'),    ('6', '대전광역시'),    ('7', '울산광역시'),    ('8', '경기도'),    ('9', '강원도'),    ('10', '충청북도'),    ('11', '충청남도'),    ('12', '전라북도'),    ('13', '전라남도'),    ('14', '경상북도'),    ('15', '경상남도'),    ('16', '제주특별자치도');
+
+#main_region_code :varchar 15로 바꿈
+
+INSERT INTO REGION_DETAIL (sub_region_code, main_region_code, sub_region_name)VALUES    ('INCHEON', 'GANGHWA', '강화군'),    ('INCHEON', 'SEO-GU', '서구'),    ('INCHEON', 'JUNG-GU', '중구'),    ('INCHEON', 'DONG-GU', '동구'),    ('INCHEON', 'ONGJIN', '옹진군'),    ('INCHEON', 'GWEWANG', '계양구'),    ('INCHEON', 'BUPUNG', '부평구'),    ('INCHEON', 'MICHUHOL', '미추홀구'),    ('INCHEON', 'NAMDONG', '남동구'),    ('INCHEON', 'YEONSU', '연수구'),    ('GYEONGGI', 'EN_YEONCHEON', '연천군'),    ('GYEONGGI', 'EN_POCHEON', '포천시'),    ('GYEONGGI', 'EN_DONGDUCHON', '동두천시'),    ('GYEONGGI', 'EN_YANGJU', '양주시'),    ('GYEONGGI', 'EN_', '의정부시'),    ('GYEONGGI', 'EN_NAMYANGJU', '남양주시'),    ('GYEONGGI', 'EN_GURI', '구리시'),    ('GYEONGGI', 'EN_GAPYEONG', '가평군'),    ('GYEONGGI', 'WN_PAJU', '파주시'),    ('GYEONGGI', 'WN_GOWANG', '고양시'),    ('GYEONGGI', 'WN_KIMPO', '김포시'),    ('GYEONGGI', 'ES_YANGPYEONG', '양평군'),    ('GYEONGGI', 'ES_GWANGJU', '광주시'),    ('GYEONGGI', 'ES_SEONGNAM', '성남시'),    ('GYEONGGI', 'ES_HANAM', '하남시'),    ('GYEONGGI', 'ES_YEOJU', '여주군'),    ('GYEONGGI', 'JU_BUCHEON', '부천시'),    ('GYEONGGI', 'JU_SIHUNG', '시흥시'),    ('GYEONGGI', 'JU_GUNPO', '군포시'),    ('GYEONGGI', 'JU_ANYANG', '안양시'),    ('GYEONGGI', 'JU_GWACHEON', '과천시');
+
+#수업스타일
+
+INSERT INTO class_style_category (style_code, style_name)
+VALUES
+    ('sty001', '열정적'),
+    ('sty002', '활발함'),
+    ('sty003', '꼼꼼함'),
+    ('sty004', '조용함'),
+    ('sty005', '섬세함');
+
+INSERT INTO UNIVERSITY (univ_code, univ_name, category_code)
+
+VALUES
+
+    ('univ_code001', '서울대학교', 'RE001'),       ('univ_code002', '고려대학교(본캠)', 'RE001'),       ('univ_code003', '연세대학교(본캠)', 'RE001'),       ('univ_code004', '서강대학교', 'RE001'),       ('univ_code005', '성균관대학교', 'RE001'),       ('univ_code006', '한양대학교', 'RE001'),       ('univ_code007', '중앙대학교', 'RE001'),       ('univ_code008', '경희대학교', 'RE001'),       ('univ_code009', '한국외국어대학교', 'RE001'),       ('univ_code010', '서울시립대학교', 'RE001'),       ('univ_code011', '국민대학교', 'RE001'),       ('univ_code012', '숭실대학교', 'RE001'),       ('univ_code013', '세종대학교', 'RE001'),       ('univ_code014', '단국대학교', 'RE001'),       ('univ_code015', '건국대학교', 'RE001'),       ('univ_code016', '동국대학교', 'RE001'),       ('univ_code017', '홍익대학교', 'RE001'),       ('univ_code018', '숙명여자대학교', 'RE001'),       ('univ_code019', '한성대학교', 'RE001'),       ('univ_code020', '서경대학교', 'RE001'),       ('univ_code021', '삼육대학교', 'RE001'),       ('univ_code022', '서울여자대학교', 'RE001'),       ('univ_code023', '이화여자대학교', 'RE001'),       ('univ_code024', '카이스트(서울캠)', 'RE001'),       ('univ_code025', '서울과학기술대학교', 'RE001'),       ('univ_code026', '서울교육대학교', 'RE001'),       ('univ_code027', '육군사관학교', 'RE001'),       ('univ_code028', '한국방송통신대학교', 'RE001'),       ('univ_code029', '한국예술종합학교(한예종)', 'RE001'),       ('univ_code030', '한국체육대학교(한체대)', 'RE001'),       ('univ_code031', '인천대학교', 'RE001'),       ('univ_code032', '한경대학교', 'RE001'),       ('univ_code033', '경인교육대학교', 'RE001'),       ('univ_code034', '한국교통대학교', 'RE001');
