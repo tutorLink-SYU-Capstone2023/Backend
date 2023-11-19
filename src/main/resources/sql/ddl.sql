89e354edee94a94a7afa5bc82c07b28465cf391b
JulienJang <big00124@naver.com>
2023-11-19 14:16:55 +0900

    ddl.sql 수정

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -53,3 +53,112 @@ CREATE TABLE `BOARD_ATTACHED_FILE` (
                                        `post_num`	INT	NOT NULL
 );
 
+-- auto-generated definition
+create table ACCEPTEDTYPE_CATEGORY
+(
+    my_key char(10)    not null
+        primary key,
+    Field  varchar(10) not null
+);
+-- auto-generated definition
+create table UNIVERSITY
+(
+    univ_code     varchar(20) not null
+        primary key,
+    univ_name     varchar(50) null,
+    category_code varchar(10) null
+);
+-- auto-generated definition
+create table member_role
+(
+    member_role_code int auto_increment
+        primary key,
+    authority_num    int null,
+    member_no        int null,
+    constraint FK_AUTHORITY_TO_MEMBER_ROLE_1
+        foreign key (authority_num) references AUTHORITY (authority_num),
+    constraint FK_MEMBER_TO_MEMBER_ROLE_1
+        foreign key (member_no) references member (member_no),
+    constraint member_role_ibfk_1
+        foreign key (authority_num) references AUTHORITY (authority_num),
+    constraint FK9b3yvy4p9bhays8diipd7qsj3
+        foreign key (authority_num) references AUTHORITY (authority_num),
+    constraint member_role_ibfk_2
+        foreign key (member_no) references member (member_no),
+    constraint FKr4x8609l32oouja8cpn0rujhp
+        foreign key (member_no) references member (member_no)
+);
+
+
+
+
+
+-- auto-generated definition
+create table AUTHORITY
+(
+    authority_num  int auto_increment
+        primary key,
+    authority_name varchar(45)   not null,
+    authority_desc varchar(4000) not null
+);
+-- auto-generated definition
+create table member
+(
+    member_no              int auto_increment
+        primary key,
+    member_id              varchar(20)                          not null,
+    member_pw              varchar(100)                         not null,
+    member_nickname        varchar(45)                          not null,
+    member_name            varchar(45)                          not null,
+    member_email           varchar(100)                         not null,
+    member_gender          char                                 not null,
+    member_birthday        date                                 not null,
+    member_enroll_date     datetime   default CURRENT_TIMESTAMP not null,
+    member_current_status  varchar(2) default 'A'               not null,
+    member_phone_number    varchar(20)                          not null,
+    tutor_school_authorize char       default 'N'               not null,
+    tutor_middle_school    varchar(45)                          null,
+    tutor_high_school      varchar(45)                          null,
+    tutor_uni              varchar(45)                          null,
+    tutor_uni_is_enrolled  char                                 null,
+    tutor_major            varchar(45)                          null,
+    tutor_major_num        double                               null,
+    tutor_authorize        varchar(45)                          null,
+    my_key                 char(10)                             null,
+    constraint fk_member_my_key
+        foreign key (my_key) references ACCEPTEDTYPE_CATEGORY (my_key),
+    constraint FKf58e132r60d5ux99si5oof12o
+        foreign key (my_key) references ACCEPTEDTYPE_CATEGORY (my_key),
+    constraint fk_member_tutor_uni
+        foreign key (tutor_uni) references UNIVERSITY (univ_code),
+    constraint FKbfepklb4jp1yplod1quq2iwe4
+        foreign key (tutor_uni) references UNIVERSITY (univ_code),
+    check (`member_gender` in (_utf8mb4\'M\',_utf8mb4\'F\')),
+	check (`member_current_status` in (_utf8mb4\'A\',_utf8mb4\'D\')),
+	check (`tutor_school_authorize` in (_utf8mb4\'Y\',_utf8mb4\'N\')),
+	check (`tutor_uni_is_enrolled` in (_utf8mb4\'Y\',_utf8mb4\'N\'))
+);
+
+
+
+
+-- auto-generated definition
+create table LIKED_MEMBERS
+(
+    id              int auto_increment
+        primary key,
+    member_id       int not null,
+    liked_member_id int not null,
+    constraint member_id
+        unique (member_id, liked_member_id),
+    constraint liked_members_ibfk_1
+        foreign key (member_id) references member (member_no),
+    constraint liked_members_ibfk_2
+        foreign key (liked_member_id) references member (member_no)
+);
+
+create index liked_member_id
+    on LIKED_MEMBERS (liked_member_id);
+
+
+
