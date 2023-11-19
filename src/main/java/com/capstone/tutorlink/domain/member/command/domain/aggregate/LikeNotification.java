9e6684dfc20bc218feb131daec595888a4eeddf4
JulienJang <big00124@naver.com>
2023-11-19 14:16:25 +0900

    소켓 구현전 세팅

---
+++ src/main/java/com/capstone/tutorlink/domain/member/command/domain/aggregate/LikeNotification.java
@@ -0,0 +1,11 @@
+package com.capstone.tutorlink.domain.member.command.domain.aggregate;
+
+public class LikeNotification {
+
+    private int senderMemberNo;
+    private int receiverMemberNo;
+    private String message;
+
+    // 생성자, 게터, 세터 등 필요한 메서드 추가
+}
+
